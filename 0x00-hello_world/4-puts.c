3a4f917e17fe1962dadf6022e35575db9a66acf5
orchwea <reubenongwae54@gmail.com>
2022-09-10 09:44:13 -0700

    Go there

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - output text
+ * description: this program prints
+ * Programming is like building a multilingual puzzle
+ * Return: 0 success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
