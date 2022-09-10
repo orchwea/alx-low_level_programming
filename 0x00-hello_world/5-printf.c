92af18307fce7048aedf90fcb4dbca34f23b66f3
orchwea <reubenongwae54@gmail.com>
2022-09-10 11:37:08 -0700

    Forward ever

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - output text
+ * description: this program prints
+ * with proper grammar, but the outcome is a piece of art,
+ * Return: 0 success
+ * */
+int main(void)
+{
+	printf("\with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
